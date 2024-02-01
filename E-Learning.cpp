ce04a2526eef9e981babb9e9a6260ee23936ae02
Bushra <L1S21BSSE0006@ucp.com>
2024-02-01 12:05:35 +0500

    User authentication feature defined

--- E-Learning.cpp
+++ E-Learning.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 class E_learning{
-void userAuthentication();
+void userAuthentication(){
+return user;
+}
 void courseManagement();
 void studentEnrollment();
 void DiscussionForum();
